ccea6671c9237de9e058c56b574c9678f7d6724f
Inigo <inigo.surguy@gmail.com>
2011-02-20 15:58:21 +0000

    Working conversion from Xom to Scala for non-namespaced content

--- src/main/scala/net/surguy/xom/XomConverter.scala
+++ src/main/scala/net/surguy/xom/XomConverter.scala
@@ -2,7 +2,7 @@ package net.surguy.xom
 
 import xml._
 import nu.xom.{Attribute => XomAttribute, Element => XomElement, Node => XomNode, Text => XomText, Comment => XomComment,
-              ProcessingInstruction => XomProcessingInstruction}
+              ProcessingInstruction => XomProcessingInstruction, Document => XomDocument}
 
 /**
  * Convert Scala XML to the XOM XML model, and vice versa.
@@ -28,9 +28,23 @@ object XomConverter {
     case PCData(text) => new XomText(text) // XOM does not support PCDATA - this is the closest alternative
     case Group(nodes) => throw new UnsupportedOperationException("Converting groups is not supported") // @todo Convert Group to NodeList?
     case Unparsed(text) => throw new UnsupportedOperationException("XOM does not support unparsed entities")
-    case _ => throw new UnsupportedOperationException("Cannot convert "+node+" type "+node.getClass.getName+" to XOM")
+    case _ => throw new UnsupportedOperationException("Cannot convert Scala "+node+" type "+node.getClass.getName+" to XOM")
   }
 
   private def prefixed(prefix: String, label: String) = if (prefix==null) label else prefix+":"+label
+
+  def toScalaXml(node: XomNode): Node = node match {
+    case n: XomDocument => toScalaXml(n.getRootElement) // @todo This is losing any non-element content e.g. an initial comment
+    case n: XomElement =>
+      val xomAttributes = for (i <- (0 until n.getAttributeCount)) yield n.getAttribute(i)
+      val attributes = xomAttributes.foldLeft(Null.asInstanceOf[MetaData])((existing, a) =>
+        Attribute(a.getNamespacePrefix, a.getLocalName, Text(a.getValue), existing ))
+      val xomChildren = for (i <- (0 until n.getChildCount)) yield toScalaXml(n.getChild(i))
+      new Elem(if (n.getNamespacePrefix=="") null else n.getNamespacePrefix, n.getLocalName, attributes, TopScope, xomChildren :_* )
+    case n: XomText => Text(n.getValue)
+    case n: XomComment => Comment(n.getValue)
+    case n: XomProcessingInstruction => ProcInstr(n.getTarget, n.getValue)
+    case _ => throw new UnsupportedOperationException("Cannot convert XOM "+node+" type "+node.getClass.getName+" to Scala XML")
+  }
 
 }
--- src/test/scala/net/surguy/xom/XomConverterSpec.scala
+++ src/test/scala/net/surguy/xom/XomConverterSpec.scala
@@ -49,12 +49,52 @@ class XomConverterSpec  extends SpecificationWithJUnit {
     }
   }
 
+  "converting from XOM to Scala XML" should {
+    "produce identical results for a single element containing text" in {
+      testScalaConversion("<root>Some elements</root>")
+    }
+    "produce identical results for multiple nested elements" in {
+      testScalaConversion("<root><b>Some</b> <i>things<span>elements</span> and so on<x><y><z/></y></x></i></root>")
+    }
+    "produce identical results for attributes" in {
+      testScalaConversion("<root a=\"1\" b=\"2\">Some <span class=\"this\">things</span></root>")
+    }
+    "produce identical results for comments" in {
+      testScalaConversion("<root>Before comment <!-- Comments <commentedOut/> --> and before empty comment <!-- --></root>")
+    }
+    "produce identical results for processing instructions" in {
+      testScalaConversion("<root>Some <?some thing?> with PIs</root>")
+    }
+    "produce identical results for namespaced elements in an unprefixed namespace" in {
+      testScalaConversion("<root xmlns=\"urn:test\">Some <b>element</b></root>")
+    }
+    "produce identical results for namespaced elements in prefixed namespaces" in {
+      testScalaConversion("<root xmlns:a=\"urn:test\">Some <a:b>element</a:b></root>")
+    }
+    "produce identical results for multiple namespaced elements in prefixed namespaces" in {
+      testScalaConversion("<root xmlns:a=\"urn:test\" xmlns:b=\"urn:test2\">Some <a:x><b:y/>element<c:z xmlns:c=\"urn:test3\"/></a:x></root>")
+    }
+    "produce identical results for namespaced attributes in prefixed namespaces" in {
+      testScalaConversion("<root xmlns:a=\"urn:test\">Some <b a:value=\"one\">element</b></root>")
+    }
+  }
+
   private def testConversion(xml: String) = {
     // We need to use ConstructingParser because XML.loadString doesn't preserve comments
     val scalaXml = ConstructingParser.fromSource(Source.fromString(xml), true).document.docElem
     val xomXml = new Builder().build(new StringReader(xml))
     val convertedToXom = XomConverter.toXom(scalaXml)
     canonical(xomXml.getRootElement) must beEqualTo(canonical(convertedToXom))
+  }
+
+  private def testScalaConversion(xml: String) = {
+    // We need to use ConstructingParser because XML.loadString doesn't preserve comments
+    val scalaXml = ConstructingParser.fromSource(Source.fromString(xml), true).document.docElem
+    val xomXml = new Builder().build(new StringReader(xml))
+    val convertedToScala = XomConverter.toScalaXml(xomXml)
+    println(convertedToScala)
+    scalaXml must beEqualTo(convertedToScala)
+    canonical(xomXml.getRootElement) must beEqualTo(canonical(XomConverter.toXom(convertedToScala)))
   }
 
   private def canonical(xml: XomNode): String = {
