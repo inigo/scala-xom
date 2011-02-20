386847d3f777cf0116be904ac82968d42ad59f70
Inigo <inigo.surguy@gmail.com>
2011-02-20 19:23:43 +0000

    Added test for applying Saxon to a converted Scala XML tree - currently failing - no Document!

---
+++ src/test/scala/net/surguy/xom/ScalaXPathSpec.scala
@@ -0,0 +1,21 @@
+package net.surguy.xom
+
+import org.specs.SpecificationWithJUnit
+import Implicits._
+
+/**
+ * Test applying XPath to Scala XML via XOM.
+ *
+ * @author Inigo Surguy
+ * @created 20/02/2011 19:18
+ */
+class ScalaXPathSpec extends SpecificationWithJUnit {
+
+  "applying XPath" should {
+    "return results when applied to converted Scala XML" in {
+      val node = <root>Here is <b>some</b> XML</root>.toXom.selectSingleNode("//b[contains(.,'s.*')]")
+      node must beSomething
+    }
+  }
+
+}
