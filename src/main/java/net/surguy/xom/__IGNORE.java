388992166082dd783a54ec94c0f475f9060666b3
Inigo <inigo.surguy@gmail.com>
2011-02-23 21:14:49 +0000

    Added dummy Java file to ensure that JavaDoc gets generated - needed for Sonatype - see https://issues.sonatype.org/browse/OSSRH-637

---
+++ src/main/java/net/surguy/xom/__IGNORE.java
@@ -0,0 +1,12 @@
+package net.surguy.xom;
+
+/**
+ * This class exists solely so Maven will create a Javadoc bundle for the project,
+ * which is required by Sonatype, so the project can be released to Maven Central.
+ * <p>
+ * This issue is <a href="https://issues.sonatype.org/browse/OSSRH-637">raised in
+ * the Sonatype tracker</a>.
+ */
+public class __IGNORE {
+	private __IGNORE() { }
+}
